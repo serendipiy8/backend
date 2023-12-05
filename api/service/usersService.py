28693e5eb96f73fdc1ea798e8805ae726a989d79
Liu Qinran <1820075491@qq.com>
2023-12-05 22:09:03 +0800

    11

--- api/service/usersService.py
+++ api/service/usersService.py
@@ -4,41 +4,41 @@
 from app import db
 from utils import commons
 from utils.loggings import loggings
-from utils.response_code import RET,error_map_EN
+from utils.response_code import RET, error_map_EN
 from utils.rsa_encryption_decryption import RSAEncryptionDecryption
 from controller.usersController import UsersController
 from dataclasses import asdict
 from utils.generate_id import GenerateID
 
+
 class UsersService(UsersController):
 
     @classmethod
-    def login(cls,**kwargs):
+    def login(cls, **kwargs):
 
-        username=kwargs.get('username')
-        password=kwargs.get('password')
+        username = kwargs.get('username')
+        password = kwargs.get('password')
 
         try:
-            user_info=db.session.query(
+            user_info = db.session.query(
                 cls.UserName,
                 cls.Password,
-            )\
-            .filter(cls.UserName==username)\
-            .first()
+            ) \
+                .filter(cls.UserName == username) \
+                .first()
 
             if not user_info:
-                return {'code':RET.LOGINERR,'message':error_map_EN[RET.LOGINERR],'error':"用户或密码错误"}
+                return {'code': RET.LOGINERR, 'message': error_map_EN[RET.LOGINERR], 'error': "用户或密码错误"}
 
             user_info = dict(user_info._asdict())
 
-
-            if not user_info['Password']==password:
-                return {'code':RET.LOGINERR,'message':error_map_EN[RET.LOGINERR],'error':"用户或密码错误"}
+            if not user_info['Password'] == password:
+                return {'code': RET.LOGINERR, 'message': error_map_EN[RET.LOGINERR], 'error': "用户或密码错误"}
 
             db.session.close()
 
             print(user_info)
-            back_dict={
+            back_dict = {
                 "username": user_info['UserName'],
             }
 
@@ -52,20 +52,20 @@ class UsersService(UsersController):
     @classmethod
     def register(cls, **kwargs):
         username = kwargs.get('username')
-        email=kwargs.get('email')
+        email = kwargs.get('email')
         password = kwargs.get('password')
-        configurePassword=kwargs.get('configurePassword')
+        configurePassword = kwargs.get('configurePassword')
 
         try:
-            if password!=configurePassword:
-                return {'code':RET.PWDERR,'message':error_map_EN[RET.PWDERR],'error':'两次输入密码不一致'}
+            if password != configurePassword:
+                return {'code': RET.PWDERR, 'message': error_map_EN[RET.PWDERR], 'error': '两次输入密码不一致'}
 
             existing_user = db.session.query(cls).filter_by(UserName=username).first()
             if existing_user:
                 return {'code': RET.DATAEXIST, 'message': error_map_EN[RET.DATAEXIST], 'error': "账户已存在"}
 
-            id=int(GenerateID.create_random_id())
-            new_user = cls(UserID=id,Email=email,UserName=username, Password=password)
+            id = int(GenerateID.create_random_id())
+            new_user = cls(UserID=id, Email=email, UserName=username, Password=password)
             # new_user.add(Account=account, Password=password)
             db.session.add(new_user)
             db.session.commit()
@@ -82,7 +82,7 @@ class UsersService(UsersController):
             db.session.close()
 
             back_dict = {
-                "email":user_info['Email'],
+                "email": user_info['Email'],
                 "username": user_info['UserName'],
             }
 
